bedf90d6c4592e299d3959003b24eb563cd0a4b7
Patrick Carlton <pcarlton73@gmail.com>
2015-07-07 18:56:34 -0500

    added the first set of choices to div#choices.

--- global.js
+++ global.js
@@ -4,26 +4,33 @@
 
 
 
-var question1 = {   question: "What are you?",
-                    possibleAnswers: "(a) human, (b) robot, (c) unicorn, (d) chicken",
+var questionArray = [
+                {   question: "What are you?",
+                    choices: ["(a) human", "(b) robot", "(c) unicorn", "(d) chicken"],
                     rightAnswer: "a"
-                };
+                },
 
-var question2 = {   question: "Where is OCS?",
-                    possibleAnswers: "(a) Omaha, (b) Elmwood, (c) Nehawka, (d) Lincoln",
+                {   question: "Where is OCS?",
+                    choices: ["(a) Omaha", "(b) Elmwood", "(c) Nehawka", "(d) Lincoln"],
                     rightAnswer: "a"
-                };
+                },
 
-var question3 = {   question: "What's the right answer?",
-                    possibleAnswers: "(a) this one, (b) not this one, (c) not this one, (d) not this one",
+                {   question: "What's the right answer?",
+                    choices: ["(a) this one", "(b) not this one", "(c) not this one", "(d) not this one"],
                     rightAnswer: "a"
-                };
+                },
 
-var question4 = {   question: "Pick something.",
-                    possibleAnswers: "(a) Stop picking a, (b) I'm bored, (c) yup, (d) Chicken-Wang",
+                {   question: "Pick something.",
+                    choices: ["(a) Stop picking a", "(b) I'm bored", "(c) yup", "(d) Chicken-Wang"],
                     rightAnswer: "a"
-                };
+                }   ];
 
 var idQuestion = document.getElementById("question");
 
-idQuestion.innerText = question1.question;
+idQuestion.innerText = questionArray[0].question;
+
+var idChoices = document.getElementById("choices");
+
+idChoices.innerText = questionArray[0].choices.join(", ");
+
+
