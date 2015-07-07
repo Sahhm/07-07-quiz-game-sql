31d0f4645497011aacf653e583fb40e21adbfc73
Sam Stephen <samueljstephen@gmail.com>
2015-07-07 17:51:58 -0500

    added a collection of questions as objects

--- global.js
+++ global.js
@@ -4,6 +4,24 @@
 
 
 
+var question1 = {   question: "What are you?",
+    possibleAnswers: "(a) human, (b) robot, (c) unicorn, (d) chicken",
+    rightAnswer: "a"
+};
 
+var question2 = {   question: "Where is OCS?",
+    possibleAnswers: "(a) Omaha, (b) Elmwood, (c) Nehawka, (d) Lincoln",
+    rightAnswer: "a"
+};
+
+var question3 = {   question: "What's the right answer?",
+    possibleAnswers: "(a) this one, (b) not this one, (c) not this one, (d) not this one",
+    rightAnswer: "a"
+};
+
+var question4 = {   question: "Pick something.",
+    possibleAnswers: "(a) Stop picking a, (b) I'm bored, (c) yup, (d) Chicken-Wang",
+    rightAnswer: "a"
+};
 
 
